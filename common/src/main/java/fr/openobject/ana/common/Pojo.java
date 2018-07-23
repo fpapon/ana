b8e8192cc55f48397a206b9a636a1667b9f54ff1
Francois Papon <fpapon@yupiik.com>
2018-07-23 20:55:26 +0400

    Init

---
+++ common/src/main/java/fr/openobject/ana/common/Pojo.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the xxx under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The xxx licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.openobject.ana.common;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * This is a Pojo view.
+ */
+public class Pojo {
+
+    /** Unique ID */
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
